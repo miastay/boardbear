109f9f625179c7fbe42e83670dab5d69c86bf176
Ryan Taylor <59121627+ryant74@users.noreply.github.com>
2022-02-01 11:24:19 -0800

    Initial app setup, primitive ws handling

--- src/app.js
+++ src/app.js
@@ -2,40 +2,65 @@ const express = require('express');
 const cors = require('cors');
 const app = express()
 bodyParser = require("body-parser");
-app.use(util.overrideContentType())
 app.use(bodyParser());
 app.use(cors());
 app.use(express.static('res'))
-const fs = require('fs');
 
-let {PythonShell} = require('python-shell')
+const os = require('os')
 
-var AWS = require('aws-sdk');
-var path = require('path');
-AWS.config.update({region: 'us-east-1'});
-var credentials = new AWS.SharedIniFileCredentials({profile: 'default'});
-AWS.config.credentials = credentials;
-s3 = new AWS.S3();
 
-var multer  = require('multer');
-var http = require('http');
-var upload = multer({ dest: '/home/ec2-user/WebApp/ffuploads' });
+const WebSocket = require('ws')
+const wss = new WebSocket.Server({ port: 8091 });
 
-app.get('/boardbear', (req, res) => {
+wss.on("connection", function connection(ws, req) {
+    
+    ws.id = wss.getUniqueID();
+
+    console.log(ws.id);
+
+    if(wss.clients.size == 1) {
+        ws.auth = "owner"
+    } else {
+        ws.auth = "guest"
+    }
+    ws.send(JSON.stringify({'auth': ws.auth, 'id': ws.id}))
+
+    ws.on("message", (msg) => {
+        [...wss.clients.keys()].forEach((client) => {
+            client.send(msg);
+        });
+    });
+  
+    ws.on("close", (ws) => {
+        if(ws.auth == "owner") {
+            [...wss.clients.keys()].forEach((client) => {
+                client.send("owner closed")
+                client.close();
+            });
+        }
+    });
+});
+wss.getUniqueID = function () {
+    function s4() {
+        return Math.floor((1 + Math.random()) * 0x10000).toString(16).substring(1);
+    }
+    return s4() + s4() + '-' + s4();
+};
+
+
+app.get('/', (req, res) => {
     const options = {
-        root: './src/'
+        root: './'
     }
     res.sendFile('home.html', options)
 })
-
 app.get('/boardbear.js', (req, res) => {
     const options = {
-        root: './src/'
+        root: './'
     }
     res.sendFile('script.js', options)
 })
-
-app.listen(8080)
+app.listen(8081)
 
 app.use(function (req, res, next) {
     res.status(x => x >= 400).redirect('/');
--- src/script.js
+++ src/script.js
@@ -1,11 +1,81 @@
+const appendMessage = (msg) => {
+    console.log(msg);
+}
 
-var sock = new SockJS('https://mydomain.com/my_prefix');
-sock.onopen = function() {
-    console.log('open');
-    sock.send('test');
-};
+window.onload = async () => {
+    const serverAddress = 'ws://3.84.15.164:8091/';
+  
+    appendMessage(`Connecting to ${serverAddress}`);
+  
+    try {
+      let retries = 0;
+      while (retries < 50) {
+        appendMessage(`establishing connection... retry #${retries}`);
+        await runSession(serverAddress);
+        await Time.sleep(1500);
+        retries++;
+      }
+  
+      appendMessage("Reached maximum retries, giving up.");
+    } catch (e) {
+      appendMessage(e.message || e);
+    }
+  };
 
-sock.onmessage = function(e) {
-    console.log('message', e.data);
-    sock.close();
-};
+  var pressed = false;
+  var lastpos = {x: 0, y: 0}
+  var id;
+  
+  async function runSession(address) {
+    const ws = new WebSocket(address);
+  
+    ws.addEventListener("open", () => {
+      appendMessage("connected to server");
+    });
+  
+    ws.addEventListener("message", ({ data }) => {
+        try {
+            data.text().then(text => {
+                console.log(JSON.parse(text));
+                draw(JSON.parse(text), lastpos);
+            });
+        } catch(err) {
+            document.getElementById("id").innerText = JSON.parse(data).auth + "_" + JSON.parse(data).id
+        }
+        
+    });
+    document.body.onmousemove = (evt) => {
+        if(pressed) {
+            const messageBody = {'last': {x: lastpos.x, y: lastpos.y }, 'new': {x: evt.clientX, y: evt.clientY }};
+            ws.send(JSON.stringify(messageBody));
+        }
+        lastpos = { x: evt.clientX, y: evt.clientY };
+    }
+    document.body.onmousedown = (evt) => { pressed = true; }
+    document.body.onmouseup = (evt) => { pressed = false; }
+    document.body.onmouseout = (evt) => { pressed = false; }
+
+    return new Promise((resolve) => {
+      ws.addEventListener("close", () => {
+        appendMessage("Connection lost with server.");
+        window.open('/', '_self')
+        resolve();
+      });
+    });
+  }
+  
+
+  ////////////
+  function draw(pos) {
+    var canvas = document.getElementById('canvas');
+    if (canvas.getContext) {
+        var ctx = canvas.getContext('2d');
+        ctx.lineWidth = 5;
+        ctx.beginPath();
+        ctx.moveTo(pos.last.x, pos.last.y)
+        ctx.strokeStyle = 'blue';
+        ctx.lineTo(pos.new.x, pos.new.y);
+        ctx.stroke();
+        console.log("drew " + pos.last.x + ", " + pos.last.y + " to " + pos.new.x + ", " + pos.new.y)
+    }
+  }
